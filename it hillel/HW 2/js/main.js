ad8f7bb4542b688ce592c2fec8a72be38fcd94c4
Sergey Voytovich <58272273+sergey83631@users.noreply.github.com>
2021-02-08 19:09:44 +0200

    change mistake

--- it hillel/HW 2/js/main.js
+++ it hillel/HW 2/js/main.js
@@ -3,32 +3,31 @@ let b = +prompt('Введите число:', '');
 let c = prompt('Введите +, введите -, введите *, введите /', '');
 let d;
 
+if(isNaN(a) || isNaN(b)){
+  d = ('Введите число:');
+}else if(!a || !b){
+  d = ('Введите что-то:')
+}else{
 switch (c){
-  case '+':
+  case '+':{
   d = a + b;
-  break;
-  case '-':
+  }break;
+  case '-':{
   d = a - b;
-  break;
-  case '*':
+  }break;
+  case '*':{
   d = a * b;
-  break;
-  case '/':
+  }break;
+  case '/':{
   d = a / b;
-  break;
+  }break;
+  default:{
+  d = ('Вы ввели неправильный знак:');
 
-  default:
-  alert('Nichego');
+}
+}
 }
 alert(d);
 
-function isNumber(value){
-return typeof value === 'number' && !isNaN(parseFloat(value)) && isFinite(value);
-}
 
-console.log(isNumber('10'))
-console.log(isNumber(0))
-console.log(isNumber(10.34))
-console.log(isNumber('hi' / 3))
-console.log(isNumber(-Infinity))
-console.log(isNumber(undefined))
+
