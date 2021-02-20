83c70c17e522682894aff34eef5f979f8d83f4aa
Sergey Voytovich <58272273+sergey83631@users.noreply.github.com>
2021-02-21 01:11:56 +0200

    Changed

--- Esnykers/New/js/main.js
+++ Esnykers/New/js/main.js
@@ -1,29 +1,41 @@
 alert('Ответ писать с большой буквы и на русском, после ответа нажимаешь Ок и будет следующий вопрос')
-let nameOfPerson = 'Рита';
+// let nameOfPerson = ('Тая', 'Рита', 'Макар' || 'тая', 'рита', 'макар');
 let name = prompt('Введите имя:');
-while (name != nameOfPerson || name == ''){
+while (name == '' || name == null){
     name = prompt('Введите имя:');
 }
 alert('Привет, ' + name);
 
-let mainDateOur = '19';
-let date = prompt('Введи число начала наших отношений:');
-while (date != mainDateOur || date == ''){
-    date = prompt('Введи число начала наших отношений:');
+let number = prompt('Я сделал игру, в которой нужно угадать число от 1 до 100. Давай посмотрим, сколько тебе нужно будет попыток) Введи число:' );
+let NumberOfGuesses = 0;
+let guess = Math.ceil(Math.random() * 100);
+while(number != guess){
+    if(number > guess){
+        number = prompt('Много! Попробуй еще раз: ');
+        NumberOfGuesses += 1;
+    }
+    if(number < guess){
+        number = prompt('Мало! Попробуй еще раз: ');
+        NumberOfGuesses += 1;
+    }
 }
-alert('Умничка! Ты близка к финишу =)');
+alert('Ты угадал(а), ' + 'я загадал ' + guess + '. Тебе понадобилось ' + NumberOfGuesses + ' попыток');
+// while (date != mainDateOur || date == ''){
+//     date = prompt('Введи число начала наших отношений:');
+// }
+// alert('Умничка! Ты близка к финишу =)');
 
-let answerFromRita = 'Да';
-let answer = prompt('Ты меня любишь?');
-while (answer != answerFromRita || answer == ''){
-    answer = prompt('Ты меня любишь?');
-}
-alert('Я тебя люблю больше!');
-alert('Давай мириться? Пожалуйста');
+// let answerFromRita = 'Да';
+// let answer = prompt('Ты меня любишь?');
+// while (answer != answerFromRita || answer == ''){
+//     answer = prompt('Ты меня любишь?');
+// }
+// alert('Я тебя люблю больше!');
+// alert('Давай мириться? Пожалуйста');
 
-let answerToInvite = 'Да';
-let inviteToCafe = prompt('Ты согласна со мной встретиться вечером?');
-while (inviteToCafe != answerToInvite || inviteToCafe == ''){
-    inviteToCafe = prompt('Ты согласна со мной встретиться вечером?');
-}
-alert('Напиши мне в телегу +, потому что я еще не успел выучить, чтобы ответ приходил мне =)');
+// let answerToInvite = 'Да';
+// let inviteToCafe = prompt('Ты согласна со мной встретиться вечером?');
+// while (inviteToCafe != answerToInvite || inviteToCafe == ''){
+//     inviteToCafe = prompt('Ты согласна со мной встретиться вечером?');
+// }
+// alert('Напиши мне в телегу +, потому что я еще не успел выучить, чтобы ответ приходил мне =)');
