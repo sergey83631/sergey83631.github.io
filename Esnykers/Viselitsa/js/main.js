b0d5875bdcd0edd8afc49a45fa158a31e811e338
Sergey Voytovich <58272273+sergey83631@users.noreply.github.com>
2021-02-21 17:52:23 +0200

    Add files via upload

---
+++ Esnykers/Viselitsa/js/main.js
@@ -0,0 +1,28 @@
+var words = ['мама', 'футбол', 'машина', 'прекрасный', 'папа', 'телефон', 'планшет', 'книга'];
+var word = words[Math.floor(Math.random() * words.length)];
+var answerArray = [];
+for (var i = 0; i < word.length; i++) {
+  answerArray[i] = '_';
+}
+
+var remainingLetters = word.length;
+while (remainingLetters > 0){
+    alert(answerArray.join(' '));
+
+var guess = prompt('Угадайте букву, или нажмите Отмена для выхода.');
+if (guess === null && guess === NaN) {
+    break;
+}else if (guess.length !== 1){
+    alert('Введите, пожалуйста, одну букву');
+}else{
+    for (var j = 0; j < word.length; j++){
+        if (word[j] === guess){
+            answerArray[j] = guess;
+            remainingLetters--;
+        }
+    }
+}
+}
+alert(answerArray.join(' '));
+
+alert('Отлично! Было загадано слово ' + word);
